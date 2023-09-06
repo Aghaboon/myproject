d2ff32e3e602eb53d2775869d5974d3d86b26712
Aghaboon <4010675@upm.edu.sa>
2023-09-07 01:53:35 +0300

    uplade Home page update

--- aghaboon/src/Home.jsx
+++ aghaboon/src/Home.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 const Home = () => {
+  const Space = '\u00A0\u00A0\u00A0\u00A0\u00A0';
     return ( 
        <div className='para'>
         <p className='font-bold text-3xl'>About Me</p>
         <p className='text-xl font-medium leading-9'>
-          I'm a Senior Software Engineering student with a strong passion for both front-end and back-end development. 
+          {Space} I'm a Senior Software Engineering student with a strong passion for both front-end and back-end development. 
           My internship at Bineyes involved creating web prototypes and developing user interfaces, where I followed Agile-Scrum methodology. 
           Currently pursuing a Bachelor's Degree in Software Engineering at Prince Mugrin University, I've earned accolades for academic excellence. 
           I'm proficient in HTML, CSS, JavaScript, and Python and have hands-on experience in various projects, from web applications to chat rooms and puzzle-solving apps. 
